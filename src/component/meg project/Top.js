12b87254bc3805d2da4498ad337f7e226fd7b212
Anoriowo <anoriowo@gmail.com>
2022-08-05 23:26:14 +0100

    making my header sticky

--- src/component/meg project/Top.js
+++ src/component/meg project/Top.js
@@ -1,29 +1,39 @@
-import React from "react";
+import {React, useState} from "react";
 import productItems from "./megdata/Topselling";
 import {Link} from 'react-router-dom'
 import { FaShoppingCart } from 'react-icons/fa';
 
 
 const Topselling = () =>{
+  const [searchTerm, setSearchTerm] = useState('')
 return(
   <div className='trend'>
   <h2>Top Selling Items</h2>
+  <input type='text' placeholder='search'  onChange={event => {setSearchTerm(event.target.value)}}/>
   <hr className="hr_one" />
   <div className='trend-box'>  
-          {
-              productItems.map((item) => {
-              return(
-                  <div key={item.id} className='trend-item'>
-                      <Link to= {`/Topselling/${item.id}`}>
-                      <img src={item.img_src} alt='the_image' />
-                      <h3>{item.text}</h3>  
-                      <p>{item.Amount}</p>
-                      </Link>                        
-                      <button className='btn'>Add to cart <FaShoppingCart/></button> 
-                  </div>
-                  )
-                                  }
-          )}
+  {
+                        productItems.filter((item) => {
+                            if (searchTerm == ''){
+                                return item
+                            }else if (item.text.toLowerCase().includes(searchTerm.toLowerCase())){
+                                return item
+                            }
+                        })
+
+                        .map((item) => {
+                        return(
+                            <div key={item.id} className='trend-item'>
+                                <Link to= {`/Topselling/${item.id}`}>
+                                <img src={item.img_src} alt='the_image' />
+                                <h3>{item.text}</h3>  
+                                <p>{item.amount}</p>
+                                </Link>                        
+                                <button className='btn'>Add to cart <FaShoppingCart/></button> 
+                            </div>
+                            )
+                                            }
+                    )}
                 
   </div>
   
